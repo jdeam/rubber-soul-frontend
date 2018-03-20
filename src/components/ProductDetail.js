5ed7fc71109492126862eab33e5a2c597dcdcedf
Angela Ahn <29741357+aahn84@users.noreply.github.com>
2018-03-20 15:03:28 -0700

    create ProductDetail file

---
+++ src/components/ProductDetail.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+const shoes = require('../seedData/shoeData.json')
+
+const ProductDetail = () => {
+  console.log(shoes);
+  return (
+    <div>
+      This is my div
+    </div>
+  )
+}
+
+export default ProductDetail
