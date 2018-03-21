2ec0a0e4d51aab4fd2d2130b1ab4e8551f14d3c3
Jordan Deam <jordan.deam@gmail.com>
2018-03-21 15:41:09 -0700

    Added basic header and footer

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { Route } from 'react-router-dom';
+import 'bulma/css/bulma.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container">
+        <Header />
+        <Footer />
       </div>
     );
   }
