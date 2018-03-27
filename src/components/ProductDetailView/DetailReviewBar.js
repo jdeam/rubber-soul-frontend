753e97f95922598d3a15d25a1dd2b3d131eef8a7
Angela Ahn <aahn84@gmail.com>
2018-03-26 23:43:47 -0700

    fix star ratings and resolve errors

--- src/components/ProductDetailView/DetailReviewBar.js
+++ src/components/ProductDetailView/DetailReviewBar.js
@@ -5,21 +5,21 @@ import DetailStars from './DetailStars'
 const DetailReviewBar = ({shoeReviews}) => {
   // console.log('!!!',shoeReviews);
   let numReviews = 0;
-  let reviewStars = 0;
+  // let reviewStars = 0;
   if (shoeReviews) {
     numReviews = shoeReviews.length;
     // reviewStars = Math.round(shoeReviews.reduce((total, review) => {
     //   return total + review.star_count;
     // }, 0) / shoeReviews.length * 2) / 2;
-    reviewStars = shoeReviews.reduce((total, review) => {
-      return total + review.star_count;
-    }, 0) / shoeReviews.length;
+    // reviewStars = shoeReviews.reduce((total, review) => {
+    //   return total + review.star_count;
+    // }, 0) / shoeReviews.length;
   }
   // console.log('STARS', reviewStars);
 
   return (
     <div id="Detail-review-bar">
-      <DetailStars rating={reviewStars} shoeReviews={shoeReviews}/>
+      <DetailStars shoeReviews={shoeReviews}/>
       <div id="Detail-number-reviews">
         <strong>{`${numReviews} Reviews`}</strong>
         {/* &nbsp; &nbsp; */}
--- src/components/ProductDetailView/DetailStars.js
+++ src/components/ProductDetailView/DetailStars.js
@@ -1,49 +1,45 @@
 import React from 'react'
+import './DetailStars.css'
 
 const DetailStars = ({ rating, shoeReviews }) => {
-  console.log('RAINT', rating);
-  console.log('REVIEWS', shoeReviews);
-  let stars = Math.round(rating * 2) / 2;
-  console.log(stars);
-  // if (rating) {
-  //   numReviews = shoeReviews.length;
-  //   reviewStars = Math.round(shoeReviews.reduce((total, review) => {
-  //     return total + review.star_count;
-  //   }, 0) / shoeReviews.length * 2) / 2;
-  // }
-
-  let starEls = [];
-  for (let i = 1; i <= 5; i++) {
-    let diff = i - rating;
-    if (i <= rating) {
-      starEls.push(<i key={i} className="fa fa-star title is-5" style={{color: '#ed6c63'}} />)
-    } else if (diff === 0.5) {
-      starEls.push(<i key={i} className="fa fa-star-half-full title is-5" style={{color: '#ed6c63'}} />)
-    } else {
-      starEls.push(<i key={i} className="fa fa-star title is-5" />)
-    }
-    // if (stars >= 1) {
-    //   starEls.push(<i className="fa fa-star title is-5" style={{color: '#ed6c63'}} />);
-    //   stars -= 1;
-    // }
-    // else if (stars > 0) {
-    //   starEls.push(<i className="fa fa-star-half-full title is-5" style={{color: '#ed6c63'}}></i>)
-    //   stars -= 0.5;
-    // }
-    // else if (stars === 0){
-    //   starEls.push(<i className="fa fa-star title is-5" />);
-    // }
+  let avgStars;
+  if (shoeReviews) {
+    avgStars = Math.round(shoeReviews.reduce((total, review) => {
+      return total + review.star_count;
+    }, 0) / shoeReviews.length * 2) / 2;
   }
+  let ratingNum = avgStars ? avgStars : rating;
+  let counter = ratingNum;
+  let starEls = [];
 
-  console.log(starEls);
+  for (let i = 1; i <= 5; i++) {
+    if (counter >= 1) {
+      starEls.push(<i key={i} className="fa fa-star title is-5" style={{color: '#ed6c63'}} />);
+      counter -= 1;
+    }
+    else if (counter > 0) {
+      starEls.push(
+        <span id="fa-stack">
+          <i key={i} id="star-left-half" className="fa fa-star-half title is-5" style={{color: '#ed6c63'}}></i>
+          <i key={i} id="star-right-half" className="fa fa-star-o title is-5"  style={{color: '#ed6c63'}} />
+        </span>
+      )
+      counter -= 0.5;
+    }
+    else if (counter === 0) {
+      starEls.push(
+        <i key={i} className="fa fa-star-o title is-5"  style={{color: '#ed6c63'}} />
+      );
+    }
+  }
+  console.log('starEls', starEls);
 
   return (
     <div id="DetailReview-starRating">
       { starEls }
-      <p>{rating}</p>
+      <p id="DetailStars-rating-num">{ ratingNum }</p>
     </div>
   )
 }
 
 export default DetailStars
-// map to create array of stars
