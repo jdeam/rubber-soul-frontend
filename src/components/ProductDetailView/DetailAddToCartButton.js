adeab29b0d077d911caa1bd0a64bf656008d997e
Angela Ahn <aahn84@gmail.com>
2018-03-27 17:00:25 -0700

    set up to save cart in local storage

--- src/components/ProductDetailView/DetailAddToCartButton.js
+++ src/components/ProductDetailView/DetailAddToCartButton.js
@@ -7,7 +7,17 @@ const addShoeToCart = (shoe_id, qty, size) => {
     qty,
     size
   }
-  console.log(cartItem);
+  // localStorage.setItem('cartItem', JSON.stringify(cartItem));
+  // let getStorage = localStorage.getItem('cartItem');
+  // console.log(getStorage);
+  let currentCartId = localStorage.getItem('cart_id');
+  if (currentCartId) {
+    //do stuff
+    console.log('I HAVE AN ID');
+  } else {
+    //do other stuff without cart id
+    console.log('I DO NOT HAVE ONE');
+  }
 }
 
 const DetailAddToCartButton = ({ shoe_id, qty, size }) => {
