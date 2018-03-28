2e39c163360bb22766eee97cc14c6a397050440b
abbotteverett <severettabbott@gmail.com>
2018-03-28 13:54:18 -0700

    Successfully implemented login - still needs connection to redux

--- src/components/Header/AccountDropdown.js
+++ src/components/Header/AccountDropdown.js
@@ -1,31 +1,67 @@
 import React from 'react';
+import axios from 'axios';
 import { Link } from 'react-router-dom';
 import './AccountDropdown.css';
 
-const AccountDropdown = () => {
+class AccountDropdown extends React.Component {
+  state = { 
+    email: '', 
+    password: ''
+  };
 
-  return (
-    <div className="navbar-item dropdown is-right is-hoverable">
-      <div className="dropdown-trigger">
-        <Link to="/account" className="navbar-item">
-        <p>My Account &nbsp;</p>
-        <i className="fa fa-user-circle title is-4" aria-hidden="true" />
-      </Link>
-    </div>
-    <div className="dropdown-menu" id="dropdown-menu6" role="menu">
-      <div className="dropdown-content">
-        <div className="dropdown-item">
-          <div className="field">
-            <p className="control has-icons-left has-icons-right">
-              <input className="input" type="email" placeholder="Email" />
-                <span className="icon is-small is-left">
-                  <i className="fas fa-envelope"></i>
-                </span>
-              </p>
-            </div>
-            <div className="field">
-              <p className="control has-icons-left">
-                <input className="input" type="password" placeholder="Password" />
+  setEmail = (e) => {
+    this.setState({ email: e.target.value });
+  }
+  setPassword = (e) => {
+    this.setState({ password: e.target.value });
+  }
+  attemptLogin = (e) => {
+    e.preventDefault();
+    if (!this.state.email.length || !this.state.password.length) {
+      console.log('Please enter all fields');
+    } else {
+      const BaseURL = 'http://localhost:8080';
+      axios.post(`${BaseURL}/user/login`, {
+        email: this.state.email,
+        password: this.state.password
+      })
+        .then(res => {
+          const token = res.headers.auth.split(' ')[1];
+          localStorage.setItem('token', token);
+          console.log(res.data.user_id, res.data.cart_id);
+          console.log(token);
+          this.setState({ email: '', password: '' });
+        })
+        .catch(err => {
+          console.log('Invalid credentials');
+        })
+    } 
+  }
+
+  render () {
+    
+    return (
+      <div className="navbar-item dropdown is-right is-hoverable">
+        <div className="dropdown-trigger">
+          <Link to="/account" className="navbar-item">
+            <p>My Account &nbsp;</p>
+            <i className="fa fa-user-circle title is-4" aria-hidden="true" />
+          </Link>
+        </div>
+        <div className="dropdown-menu" id="dropdown-menu6" role="menu">
+          <div className="dropdown-content">
+            <div className="dropdown-item">
+              <div className="field">
+                <p className="control has-icons-left has-icons-right">
+                  <input onChange={this.setEmail} value={this.state.email} className="input" type="email" placeholder="Email" />
+                  <span className="icon is-small is-left">
+                    <i className="fas fa-envelope"></i>
+                  </span>
+                </p>
+              </div>
+              <div className="field">
+                <p className="control has-icons-left">
+                  <input onChange={this.setPassword} value={this.state.password} className="input" type="password" placeholder="Password" />
                   <span className="icon is-small is-left">
                     <i className="fas fa-lock"></i>
                   </span>
@@ -33,9 +69,9 @@ const AccountDropdown = () => {
               </div>
               <div className="field">
                 <p className="control sign-up-field">
-                  <button className="button is-light">
+                  <button onClick={(e) => this.attemptLogin(e)} className="button is-light">
                     Login
-                  </button>
+                    </button>
                   <button className="button is-text is-small">
                     Create an account
                   </button>
@@ -46,6 +82,7 @@ const AccountDropdown = () => {
         </div>
       </div>
     );
-  }
+  } 
+}
 
-  export default AccountDropdown;
+export default AccountDropdown;
