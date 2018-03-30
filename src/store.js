990e7e1c433f23a9b46a2e869dfa47bb2fadf876
Jordan Deam <jordan.deam@gmail.com>
2018-03-30 11:07:33 -0700

    Fixing redux logger issue

--- src/store.js
+++ src/store.js
@@ -2,22 +2,24 @@ import thunkMiddleware from 'redux-thunk';
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './reducers';
-// import { createLogger } from 'redux-logger';
-// import { SHOE_MOUSED_ON, SHOE_MOUSED_OFF } from './actions';
+import { createLogger } from 'redux-logger';
+import { SHOE_MOUSED_ON, SHOE_MOUSED_OFF } from './actions';
 
-// const logger = createLogger({
-//   predicate: (getState, action) => (
-//     action.type !== SHOE_MOUSED_ON && action.type !== SHOE_MOUSED_OFF
-//   )
-// });
+const logger = createLogger({
+  predicate: (getState, action) => (
+    action.type !== SHOE_MOUSED_ON && action.type !== SHOE_MOUSED_OFF
+  )
+});
+
+const middlewares = [ thunkMiddleware ];
+if (process.env.NODE_ENV === 'development') {
+  middlewares.push(logger);
+}
 
 const store = createStore(
   rootReducer,
   composeWithDevTools(
-    applyMiddleware(
-      thunkMiddleware,
-      // logger
-    )
+    applyMiddleware(...middlewares)
   )
 );
 
